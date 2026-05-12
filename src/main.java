d801100199e610d7430a3d310656dcaa53b7e64a
Hugo <hugoedmundo563@gmail.com>
2026-05-11 21:35:35 -0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class main {
+
+
+    public static void main(String[] args) {
+       int op;
+        do {
+           Scanner scanner = new Scanner (System.in);
+        System.out.println("====================Bem vindo ao Randomizador====================");
+        System.out.println("1- Adicionar jogo na lista");
+        System.out.println("2- Mostrar os jogos da lista");
+        System.out.println("3- Escolher jogo aleatório");
+        System.out.println("4- Remover jogo da lista");
+        System.out.println("5- Encerrar programa");
+        System.out.println("=================================================================");
+        System.out.println("Escolha uma das opções acima");
+        op = scanner.nextInt();
+       }  while (op != 5);     
+
+        switch (op) {
+            case 1: 
+                
+        }
+    }
+    
+}
