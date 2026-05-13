0376d5be38fdd8fe56f0be4d811949d44588ad7b
Hugo <hugoedmundo563@gmail.com>
2026-05-13 16:21:17 -0300

    feat: adiciona cadastro e listagem de jogos

--- src/main.java
+++ src/main.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
 public class main {
-
-
     public static void main(String[] args) {
        int op;
+        int i;
+       String jogos;
+       ArrayList<String> lista = new ArrayList<>();
+        Scanner scanner = new Scanner (System.in);
         do {
-           Scanner scanner = new Scanner (System.in);
         System.out.println("====================Bem vindo ao Randomizador====================");
         System.out.println("1- Adicionar jogo na lista");
         System.out.println("2- Mostrar os jogos da lista");
@@ -16,12 +17,31 @@ public class main {
         System.out.println("=================================================================");
         System.out.println("Escolha uma das opções acima");
         op = scanner.nextInt();
-       }  while (op != 5);     
+        scanner.nextLine();
+       
 
         switch (op) {
             case 1: 
-                
+                do{
+                System.out.println("=========Adicionar jogos=========");
+                System.out.println("Digite o jogo que deseja adicionar na lista: ");
+                jogos = scanner.nextLine();
+                lista.add(jogos);
+                    System.out.println("Deseja adicionar mais um jogo?");
+                    System.out.println("1- Sim");
+                    System.out.println("2- Não(sair)");
+                    i = scanner.nextInt();
+                    scanner.nextLine();
+           } while(i != 2);
+                 break;
+           case 2:
+               for(i = 0; i < lista.size(); i++)
+               System.out.println(lista.get(i));
+                break;
         }
-    }
+       
+        
+    }  while (op != 5);     
+        }
     
 }
